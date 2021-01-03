e157d429d2cd46e6547d04a6d183ca90102c3841
Chris Armstrong <chrisarmstrong@fb.com>
2021-01-03 10:29:34 -0500

    add wallpaper, misc other tweaks

--- config/config.js
+++ config/config.js
@@ -10,18 +10,18 @@
 
  var config = {
 	address: "localhost", 	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
-							port: 8080,
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out or empty, is "localhost"
+	port: 8080,
 	basePath: "/", 	// The URL path where MagicMirror is hosted. If you are using a Reverse proxy
-					// you must set the sub path here. basePath must end with a /
+	// you must set the sub path here. basePath must end with a /
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
-															// or add a specific IPv4 of 192.168.1.5 :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
 	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
@@ -55,9 +55,9 @@
 		module: "calendar",
 		header: "Up Next",
 		position: "top_left",
-		maximumEntries: 7,
-		maximumNumberOfDays: 14,
 		config: {
+			maximumEntries: 7,
+			maximumNumberOfDays: 5,
 			calendars: [
 			{
 				url: "https://calendar.google.com/calendar/ical/chris.r.armstrong%40gmail.com/private-00bfcc38df412610c96e7685f031bd69/basic.ics"
@@ -66,11 +66,11 @@
 				symbol: "heart",
 				url: "https://calendar.google.com/calendar/ical/ac40cikivmgug6mdb8lmf4m3tk%40group.calendar.google.com/private-0348d12ebf539733833a87f4c9b60e44/basic.ics"
 			},
-			// {
-			// 	symbol: "facebook",
-			// 	color: "#3b5998",
-			// 	url: "https://outlook.office365.com/owa/calendar/b974be8f6e6d4e56bdea5191d032c1c1@fb.com/e4239f2d341441ba957e29a9952d8af912834209555042456892/calendar.ics"
-			// }
+			{
+				symbol: "facebook",
+				color: "#3b5998",
+				url: "https://outlook.office365.com/owa/calendar/b974be8f6e6d4e56bdea5191d032c1c1@fb.com/e4239f2d341441ba957e29a9952d8af912834209555042456892/calendar.ics"
+			}
 			]
 		}
 	},
@@ -124,32 +124,54 @@
 			refreshToken: "AQA6cpJHF7pa72yrpu8q2PNjILJz7hzzCKvgBJTY9K-YKnQmK7zUkP9wKlAyzhA1_9DpKqsBXU_hmvClQHDnIYMhq3JrgOaim936Q4uS3OZQ-H7vobPX3RSzDIBN-FNUu_A"
 		}
 	},
-	// {
-	// 	module: "MMM-GooglePhotos",
-	// 	position: "middle_center",
-	// 	config: {
-	// 	albums: ["Best of Japan"], // Set your album name. like ["My wedding", "family share", "Travle to Paris"]
-	// 	updateInterval: 1000 * 60, // minimum 10 seconds.
-	// 	sort: "random", // "old", "random"
-	// 	uploadAlbum: null, // Only album created by `create_uploadable_album.js`.
-	// 	condition: {
-	// 		fromDate: null, // Or "2018-03", RFC ... format available
-	// 		toDate: null, // Or "2019-12-25",
-	// 		minWidth: null, // Or 400
-	// 		maxWidth: null, // Or 8000
-	// 		minHeight: null, // Or 400
-	// 		maxHeight: null, // Or 8000
-	// 		minWHRatio: null,
-	// 		maxWHRatio: null,
-	// 		// WHRatio = Width/Height ratio ( ==1 : Squared Photo,   < 1 : Portraited Photo, > 1 : Landscaped Photo)
-	// 	},
-	// 	showWidth: 1080, // These values will be used for quality of downloaded photos to show. real size to show in your MagicMirror region is recommended.
-	// 	showHeight: 1920,
-	// 	timeFormat: "YYYY/MM/DD HH:mm", // Or `relative` can be used.
-	// }
-},
-]
-};
+	{
+		module: 'MMM-Todoist',
+			position: 'bottom_right',	// This can be any of the regions. Best results in left or right regions.
+			header: "Today's tasks", // This is optional
+			config: { // See 'Configuration options' for more information.
+			hideWhenEmpty: false,
+			accessToken: '254f65ddc710bcc92a5187ba210a1f9715bb0a35',
+			maximumEntries: 5,
+				updateInterval: 10 * 60 * 1000, // Update every 10 minutes
+				fade: true,
+				// projects and/or labels is mandatory:
+				projects: [1513458321],
+				showDueDate: false
+			}
+		},
+		{
+			module: "MMM-Wallpaper",
+			position: "fullscreen_below",
+			config: { // See "Configuration options" for more information.
+			  source: "/r/Amoledbackgrounds",
+			  slideInterval: 60 * 1000 // Change slides every minute
+			}
+		  }
+		// {
+		// 	module: "MMM-GooglePhotos",
+		// 	position: "middle_center",
+		// 	config: {
+		// 	albums: ["Best of Japan"], // Set your album name. like ["My wedding", "family share", "Travle to Paris"]
+		// 	updateInterval: 1000 * 60, // minimum 10 seconds.
+		// 	sort: "random", // "old", "random"
+		// 	uploadAlbum: null, // Only album created by `create_uploadable_album.js`.
+		// 	condition: {
+		// 		fromDate: null, // Or "2018-03", RFC ... format available
+		// 		toDate: null, // Or "2019-12-25",
+		// 		minWidth: null, // Or 400
+		// 		maxWidth: null, // Or 8000
+		// 		minHeight: null, // Or 400
+		// 		maxHeight: null, // Or 8000
+		// 		minWHRatio: null,
+		// 		maxWHRatio: null,
+		// 		// WHRatio = Width/Height ratio ( ==1 : Squared Photo,   < 1 : Portraited Photo, > 1 : Landscaped Photo)
+		// 	},
+		// 	showWidth: 1080, // These values will be used for quality of downloaded photos to show. real size to show in your MagicMirror region is recommended.
+		// 	showHeight: 1920,
+		// 	timeFormat: "YYYY/MM/DD HH:mm", // Or `relative` can be used.
+		// }
+		]
+	};
 
-/*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+	/*************** DO NOT EDIT THE LINE BELOW ***************/
+	if (typeof module !== "undefined") { module.exports = config; }
